97bff3d13f81f7e55efe1f50451239e4efe99880
YechanYun <53812519+yyc9920@users.noreply.github.com>
2025-12-26 17:30:12 +0900

    Fix base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
   plugins: [react()],
-  base: '/polyglot-app/', // 저장소 이름과 똑같이 적어주세요!
+  base: '/polygot-app/', // 저장소 이름과 똑같이 적어주세요!
 })
